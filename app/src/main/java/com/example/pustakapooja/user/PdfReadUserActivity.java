93453916fae86539ebdf729622d29f6f2eabd13b
Kasun Jayasinghe <74439310+kasunjayasingha@users.noreply.github.com>
2023-05-14 19:39:57 +0530

    Add favourite option

---
+++ app/src/main/java/com/example/pustakapooja/user/PdfReadUserActivity.java
@@ -0,0 +1,14 @@
+package com.example.pustakapooja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PdfReadUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf_read_user);
+    }
+}
